f9c0b9da4ef03883a7dd5ed0efff54a3706cd6e7
vlad <redrik2040@gmail.com>
2014-04-04 22:58:58 +0400

    Its work

---
+++ test.java
@@ -0,0 +1,23 @@
+class HanoyTower
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+        printStep(4, 1, 3, 2);
+    }
+    public static void printStep(int rings, int from, int to, int buffer)
+    {
+    	if(rings <= 1)
+    	{
+    		System.out.print(from);
+    		System.out.print(" -> ");
+    		System.out.println(to);
+    	}
+    	else
+    	{
+    		printStep(rings - 1, from, buffer, to);
+    		printStep(1, from, to, buffer);
+    		printStep(rings - 1, buffer, to, from);
+    	}
+    }
+}
