27700bc1bf7bf7c72d103500a7768aa5fb6bcfb8
Redrik <redrik2040@gmail.com>
2014-04-04 23:21:24 +0400

    rename

---
+++ HanoiTower.java
@@ -0,0 +1,22 @@
+class HanoiTower
+{
+    public static void main(String[] args) 
+    {
+        printStep(4, 1, 3, 2);
+    }
+    public static void printStep(int rings, int from, int to, int buffer)
+    {
+    	if(rings <= 1)
+    	{
+    		System.out.print(from);
+    		System.out.print(" -> ");
+    		System.out.println(to);
+    	}
+    	else
+    	{
+    		printStep(rings - 1, from, buffer, to);
+    		printStep(1, from, to, buffer);
+    		printStep(rings - 1, buffer, to, from);
+    	}
+    }
+}
